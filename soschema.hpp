4ccdc58a68c5199fa3e45c487f9cce4bf6c54d75
Stephan Hohe <sth.dev@tejp.de>
2018-02-28 19:21:24 +0000

    Fixed data type of UserDisplayName

--- soschema.hpp
+++ soschema.hpp
@@ -80,7 +80,7 @@ const column_spec comments_columns[] =
 		 {"Text",           CT_TEXT},
 		 {"CreationDate",   CT_DATE},
 		 {"UserId",         CT_INT},
-		 {"UserDisplayName", CT_INT},
+		 {"UserDisplayName", CT_VCHR64},
 		 {0}};
 const table_spec comments_table = {"Comments", comments_columns};
 
